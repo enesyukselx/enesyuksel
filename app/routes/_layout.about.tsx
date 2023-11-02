971719b30bca2a3097b2e608e9096a48f7bce7c4
Enes Yüksel <enesyuksel9903@gmail.com>
2023-11-02 15:57:29 +0300

    /about/

--- app/routes/_layout.about.tsx
+++ app/routes/_layout.about.tsx
@@ -1,5 +1,31 @@
+import type { MetaFunction } from "@remix-run/node";
+import HomeLayout from "~/components/Layout/HomeLayout";
+
+export const meta: MetaFunction = () => {
+    return [
+        {
+            title: "About",
+        },
+        {
+            description: "About Me",
+        },
+    ];
+};
+
 const About = () => {
-    return <div>About</div>;
+    return (
+        <HomeLayout>
+            <div className="leading-snug">
+                <h1 className="text-3xl font-bold mb-6">About</h1>
+                <p className="text-lg font-bold">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                    Vero voluptatem qui accusamus laboriosam possimus, placeat
+                    eaque aperiam molestiae maxime? Alias possimus eum, eveniet
+                    dolore laboriosam necessitatibus quos mollitia aliquid ex.
+                </p>
+            </div>
+        </HomeLayout>
+    );
 };
 
 export default About;
